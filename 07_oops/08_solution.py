8bd6dcca01248159ba7d2dd20027bf6b04c6ae24
murali <muralinetha38@gmail.com>
2024-10-31 17:04:22 +0530

    python_practice

---
+++ 07_oops/08_solution.py
@@ -0,0 +1,14 @@
+# ploymorphism
+
+class Dog:
+    def eat(self):
+        print("eating dog food")
+class Cat:
+    def eat(self):
+        print("eating cat food")
+        
+animal1 = Dog()
+animal2 = Cat()
+
+animal1.eat()
+animal2.eat()
